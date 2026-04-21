deb857758d38f53265868a24deec34b30ae4646f
Ebu3v <chukwudebeluchukwuebuka17@gmail.com>
2026-04-21 23:04:56 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const KEY = 'e1f991c0-221f-43d5-b2c1-e238c3973537';
+export const MODAL_CLOSE_SEC = 2.5;
