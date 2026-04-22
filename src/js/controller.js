6b992ef6b61a1ce2563593ceae536293e632e254
Ebu3v <chukwudebeluchukwuebuka17@gmail.com>
2026-04-22 13:29:01 +0100

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -121,6 +121,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -129,7 +133,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 
 init();
